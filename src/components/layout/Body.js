c02c0de50daac9c55eb81a46d9e20b07001523e7
이병현 <sumia1469@gmail.com>
2025-01-10 18:00:08 +0900

    개선

--- src/components/layout/Body.js
+++ src/components/layout/Body.js
@@ -1,14 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Box } from "@mui/material";
 import LNB from "./LNB";
 import MDIContents from "./MDIContents";
 
 const Body = ({ menuData, isLnbOpen }) => {
   const [activeMenu, setActiveMenu] = useState(null);
+  const [key, setKey] = useState(0); // 추가: 강제 업데이트를 위한 키 값
 
   const handleMenuClick = (menu) => {
     setActiveMenu(menu);
   };
+
+  // 추가: activeMenu 값이 변경될 때마다 키 값을 증가시켜 강제 업데이트
+  useEffect(() => {
+    setKey((prevKey) => prevKey + 1);
+  }, [activeMenu]);
 
   return (
     <Grid container sx={{ height: "100%" }}>
@@ -24,7 +30,7 @@ const Body = ({ menuData, isLnbOpen }) => {
             overflow: 'hidden'
           }}
         >
-          <MDIContents activeMenu={activeMenu}/>
+          <MDIContents key={key} activeMenu={activeMenu}/>
         </Box>
       </Grid>
     </Grid>
